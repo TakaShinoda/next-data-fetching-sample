045b82da91a0a744bee097c8ce362c0c9160b10f
Takahiro <tkhr_621@docomo.ne.jp>
2022-04-06 01:52:09 +0900

    サンプルAPIを作成

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  date: string
+  img: string
 }
+
+const date = new Date().toLocaleString()
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200)
+  .json({
+    date,
+    img: 'https://3.bp.blogspot.com/-Ml2DT77qxNk/WsiRuwuwAAI/AAAAAAABLIw/EdyR2igaIbY-LjuJ5_w7MLScO-Zc-eB2gCLcBGAs/s400/character_mezamashidokei.png'
+  })
 }

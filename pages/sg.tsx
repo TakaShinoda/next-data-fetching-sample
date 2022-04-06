7f2ffdbfb6d5505d8b02eef728aba5ec4b47f05a
Takahiro <tkhr_621@docomo.ne.jp>
2022-04-07 04:56:44 +0900

    create sg page

--- pages/sg.tsx
+++ pages/sg.tsx
@@ -1,7 +1,38 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 
-const Sg: NextPage = () => {
-  return <div className="bg-white py-6 sm:py-8 lg:py-12">sg</div>
+const Sg = ({data}: any) => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+        <img
+          className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
+          alt="mugiwara-pirates"
+          src={data.img}
+        />
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            {data.date}
+          </h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  if (process.env.NODE_ENV === 'production') {
+    // 本番環境
+    const res = await fetch(
+      'https://next-data-fetching-sample.vercel.app/api/hello'
+    )
+    const data = await res.json()
+    return { props: { data } }
+  } else {
+    // 開発環境
+    const res = await fetch('http://localhost:3000/api/hello')
+    const data = await res.json()
+    return { props: { data } }
+  }
 }
 
 export default Sg

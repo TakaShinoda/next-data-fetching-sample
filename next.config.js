635f145b6874a95fa5734c655b4b096bbfe7e5b0
Takahiro <tkhr_621@docomo.ne.jp>
2022-04-07 05:22:01 +0900

    next/image で外部ドメインを設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['1.bp.blogspot.com'], // 外部ドメインを設定
+  },
 }
 
 module.exports = nextConfig
